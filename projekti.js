21bc45ccc9a506212837db98b5c2f32088c51dc4
eridjon92 <eridjon.senka92@gmail.com>
2023-12-04 12:22:38 +0100

    change css, fix js, rename html

--- projekti.js
+++ projekti.js
@@ -1,16 +1,36 @@
 
 console.log('hello')
 
-const header = document.querySelector("header");
-
-window.addEventListener ("scroll",function() {
-    header.classList.toggle ("sticky", this.window.scrollY > 0);
-});
-
-let menu = document.querySelector('#menu-icon')
-let main = document.querySelector('.main')
-
-menu.onclick = () => {
-    menu.classList.toggle('bx-x')
-    main.classList.toggle('open')
+// Navbar responsive
+function Navbar(){
+    let nav = document.getElementById("nav")
+    if(nav.style.display =="block"){
+        nav.style.display = "none"
+    } else {
+        nav.style.display ="block"
+    }
 }
+
+// Color navbar
+
+const main = document.getElementsByClassName("main")[0];
+console.log(main)
+window.onscroll = function(){
+    let scroll = window.pageYOffset
+    if (scroll > 300){
+        main.style.backgroundColor = "black"
+    }else{
+        main.style.backgroundColor = "transparent"
+    }
+}
+// window.addEventListener ("scroll",function() {
+//     header.classList.toggle ("sticky", this.window.scrollY > 0);
+// });
+
+// let menu = document.querySelector('#menu-icon')
+// let main = document.querySelector('.main')
+
+// menu.onclick = () => {
+//     menu.classList.toggle('bx-x')
+//     main.classList.toggle('open')
+// }
